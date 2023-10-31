948476a62709c6a79a7241b423e7c0978f91a2a2
ivanhack34 <ivanpolanco34@gmail.com>
2023-10-30 21:29:53 -0400

    Aqui el inicio de los cambios con los forms mencionado por la ppt y un boton de salida. Solo los forms sin nada.

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,10 +1,38 @@
 namespace WinFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class frmInventario : Form
     {
-        public Form1()
+        public frmInventario()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalida_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnArticulos_Click(object sender, EventArgs e)
+        {
+            frmArticulos formArticulo = new frmArticulos();
+            formArticulo.Show();
+        }
+
+        private void btnTiposInventarios_Click(object sender, EventArgs e)
+        {
+            frmTiposInventarios formTipoInventario = new frmTiposInventarios();
+            formTipoInventario.Show();
+        }
+
+        private void btnAlmacenes_Click(object sender, EventArgs e)
+        {
+            frmAlmacenes formAlmacen = new frmAlmacenes();
+            formAlmacen.Show();
         }
     }
 }
